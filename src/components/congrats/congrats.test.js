6a7e31c62644017fb3562b8bea9cff5f540679a8
Mor Mordechai Ben <31767946+morMBen@users.noreply.github.com>
2021-08-23 12:42:36 +0300

    end of section 3

--- src/components/congrats/congrats.test.js
+++ src/components/congrats/congrats.test.js
@@ -1,6 +1,6 @@
 import { shallow, } from 'enzyme';
 
-import { findByTestAttr, checkProps } from '../../test/testUtils';
+import { findByTestAttr, checkProps } from '../../../test/testUtils';
 import Congrats from './congrats';
 
 
